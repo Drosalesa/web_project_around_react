96501c802cc1e64b0108146830cf7c6e7ff5b740
Dave <david.rosales1698@gmail.com>
2026-09-02 17:59:36 -0600

    Added EditAvatar component

---
+++ src/components/Main/Popup/EditAvatar/EditAvatar.tsx
@@ -0,0 +1,22 @@
+function EditAvatar(): React.JSX.Element {
+  return (
+    <form className='popup__form' id='edit-avatar-form' name='edit-avatar-form' noValidate>
+      <label className='popup__field'>
+        <input
+          id='avatar'
+          className='popup__input popup__input_type_url'
+          name='avatar'
+          placeholder='Enlace a la imagen'
+          required
+          type='url'
+        />
+        <span className='popup__error popup__error_avatar'></span>
+      </label>
+      <button className='button popup__button' type='submit' disabled>
+        Guardar
+      </button>
+    </form>
+  );
+}
+
+export default EditAvatar
