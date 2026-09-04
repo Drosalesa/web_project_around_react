6ac7bad261dafe66071ebdad7647532e08db9e97
Dave <david.rosales1698@gmail.com>
2026-09-04 16:25:43 -0600

    Fixed to show on Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/web_project_around_react/",
   server: {
     port: 3000,
   },
